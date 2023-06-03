60ee3dde9d0e3e8c5de1800df98491e8a3348b52
Pravin Sapkal <pravin.sapkal@v2solutions.com>
2023-06-03 14:00:29 +0530

    Update pets.service.ts
    use relative path

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -3,8 +3,8 @@ import { Pet } from './pet.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePetInput } from './dto/create-pet.input';
-import { OwnersService } from 'src/owners/owners.service';
-import { Owner } from 'src/owners/entities/owner.entity';
+import { OwnersService } from '../owners/owners.service';
+import { Owner } from '../owners/entities/owner.entity';
 
 @Injectable()
 export class PetsService {
