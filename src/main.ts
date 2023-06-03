d542d889c9b27170ff6e9412580e670aab1de432
Pravin Sapkal <pravin.sapkal@v2solutions.com>
2023-06-03 16:29:13 +0530

    Update main.ts
    credentials false

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({  // wrong!  in my case, anyway
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-    credentials: true,
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Accept,Authorization,Access-Control-Allow-Origin',
+    credentials: false,
   });
   app.setGlobalPrefix("api");
   await app.listen(process.env.PORT || 3002);
