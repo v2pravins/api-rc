e050f8f33b62cf8acf653ef31d6c5df9e42e1d36
Pravin Sapkal <pravin.sapkal@v2solutions.com>
2023-06-03 16:19:07 +0530

    Update main.ts
    all origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({  // wrong!  in my case, anyway
-    origin: 'https://revcon-init.herokuapp.com',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
     credentials: true,
