fd51b9541c32e87d1082c9dc2fbbc91087da25d7
Pravin Sapkal <pravin.sapkal@v2solutions.com>
2023-06-03 16:01:24 +0530

    Update main.ts
    Enabling http cors for graphql

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({  // wrong!  in my case, anyway
+    origin: 'https://revcon-init.herokuapp.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
   app.setGlobalPrefix("api");
   await app.listen(process.env.PORT || 3002);
   console.log(`Application is running on: ${await app.getUrl()}`);
